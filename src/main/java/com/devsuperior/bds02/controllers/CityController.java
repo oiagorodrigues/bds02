8f3c5b6593300b2cdd1d021ee1b774475d87afd7
Iago Rodrigues <yagomello16@gmail.com>
2021-06-16 15:18:06 -0300

    feat: add Event | City controllers

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -0,0 +1,34 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
+public class CityController {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CityDTO> findAll() {
+        return service.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
